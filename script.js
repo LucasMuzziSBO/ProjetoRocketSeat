eea0379236d8936ab8ad6260147e515ddb91837c
Lucas Muzzi <lucas_muzzi@hotmail.com>
2023-10-07 14:40:46 -0300

    :tadda: My First Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleTheme() {
+    const html = document.documentElement
+
+    html.classList.toggle('light')
+
+   const img = document.querySelector ('#profile img')
+
+    if(html.classList.contains('light')){
+        img.setAttribute ('src', 'assets/avatar-light.png');
+    } else(img.setAttribute('src', 'assets/avatar-dark.png'))
+
+    if(html.classList.contains('light')){
+        img.setAttribute('alt', 'Foto Light')
+    }else(img.setAttribute('alt', 'foto dark'))
+}
